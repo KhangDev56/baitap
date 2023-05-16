f5736399a1bdf5e613fe4dd9a98e059a5bdc6d65
KhangDev56 <133736216+KhangDev56@users.noreply.github.com>
2023-05-16 11:42:04 +0700

    Add files via upload
    first commit

---
+++ baitap/baitap/baitap/src/main/java/com/nhom11/baitap/CourseService.java
@@ -0,0 +1,16 @@
+package com.nhom11.baitap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+@Service
+public class CourseService {
+	private List<Course> listCourse = new ArrayList<>() ;
+	public void add(Course newproduct) {
+		listCourse.add(newproduct);
+	}
+	public List<Course> getAll(){
+		return listCourse;
+	}
+}
